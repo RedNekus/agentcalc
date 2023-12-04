4a2ea294a69438ebf2c81801666327b3ea7e4038
RedNekus <fighterneko@gmail.com>
2023-12-04 13:26:17 +0300

    04.12.2023 finish agent calc

--- media/js/calculate.js
+++ media/js/calculate.js
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Content-Type': 'application/json'
             },
             data:  JSON.stringify({
-                'price': formData.get('jform[price]'),
-                'prepayment': formData.get('jform[prepayment]')
+                'price': formData.get('jform[price]').replace(' ', ''),
+                'prepayment': formData.get('jform[prepayment]').replace(' ', '')
             }),
             onSuccess: (response) => {
                 const result = JSON.parse(response);
