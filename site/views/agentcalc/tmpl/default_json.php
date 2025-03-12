9cae7a8780d74c107e8775e26084739ca6f5d2a9
RedNekus <fighterneko@gmail.com>
2025-03-12 10:54:11 +0300

    12.03.2025

--- site/views/agentcalc/tmpl/default_json.php
+++ site/views/agentcalc/tmpl/default_json.php
@@ -5,7 +5,7 @@ if((int)$this->data['price']):
     <?php foreach ($this->items as $item) :
         $payment =((float)$this->data['price'] - (float)$this->data['prepayment'])*(1 + $item->remuneration*.01);
     ?>
-        <?php if( (int)$item->term <= 36 || (int)$this->data['price'] > 3000): ?>
+        <?php if( (int)$item->term <= 36 || (int)$this->data['price'] >= 3000): ?>
             <tr>
                 <td><?=$item->term?></td>
                 <td><?=number_format($this->data['price'], 2,"."," ")?></td>
@@ -19,7 +19,7 @@ if((int)$this->data['price']):
     <?php foreach ($this->items as $item) {
         $payment = $this->data['payment'];
         $price = (float)$this->data['prepayment'] + $item->term * $payment / (1 + $item->remuneration*.01);
-        if ((int)$item->term <= 36 || $price > 3000) {
+        if ((int)$item->term <= 36 || $price >= 3000) {
             echo "<tr>
                 <td>{$item->term}</td>
                 <td>" . number_format($price, 2, ".", " ") . "</td>
